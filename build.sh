d2a1802f06217b206461a1713fb16e6f5c8c2354
Ryan Flett <ryan.flett@megaport.com>
2020-04-09 19:29:56 +1000

    Add createInvite

--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ GOOS=linux go build -ldflags="-s -w" -o bin/createGroup rest/group/createGroup/m
 GOOS=linux go build -ldflags="-s -w" -o bin/getGroup rest/group/getGroup/main.go
 GOOS=linux go build -ldflags="-s -w" -o bin/getGroupMembers rest/group/getGroupMembers/main.go
 GOOS=linux go build -ldflags="-s -w" -o bin/updateGroup rest/group/updateGroup/main.go
+GOOS=linux go build -ldflags="-s -w" -o bin/createInvite rest/invites/createInvite/main.go
---
+++ rest/invites/createInvite/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/skip2/go-qrcode"
+)
+
+const inviteLink = "https://www.fuckYouNorto.com/invite?code=%s"
+
+// RequestDto is the expected body in the request
+type RequestDto struct {
+	UserID string `json:"userId"`
+}
+
+// ResponseDto is the response that we'll send back
+type ResponseDto struct {
+	Link   string `json:"link"`
+	QRCode string `json:"qrCode"`
+}
+
+// Handler is our handle on life
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	// get inviteCode from request
+	requestDto := RequestDto{}
+	err := json.Unmarshal([]byte(request.Body), &requestDto)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 400}, nil
+	}
+
+	// make link
+	link := fmt.Sprintf(inviteLink, requestDto.UserID)
+
+	// make qr code
+	qrCode, err := qrcode.Encode(link, qrcode.Low, 256)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 400}, nil
+	}
+
+	// create and send the response
+	responseDto := ResponseDto{
+		Link:   link,
+		QRCode: base64.StdEncoding.EncodeToString(qrCode),
+	}
+	body, _ := json.Marshal(responseDto)
+	headers := map[string]string{"Content-Type": "application/json"}
+	return events.APIGatewayProxyResponse{Body: string(body), StatusCode: 200, Headers: headers}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
