bc942d51dc9164ceb6e34485539f6bd6695a7a55
Ryan Flett <ryan.flett1@gmail.com>
2021-02-14 22:19:14 +1000

    Change leave group to DELETE

--- rest/group/leaveGroup/main.go
+++ rest/group/leaveGroup/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"jjj.rflett.com/jjj-api/types/group"
 	"net/http"
 
@@ -9,24 +8,14 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// requestBody is the expected body of the create groupOld request
-type requestBody struct {
-	UserID  string `json:"userID"`
-	GroupID string `json:"groupID"`
-}
-
 // Handler is our handle on life
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-	// unmarshall request body to requestBody struct
-	reqBody := requestBody{}
-	jsonErr := json.Unmarshal([]byte(request.Body), &reqBody)
-	if jsonErr != nil {
-		return events.APIGatewayProxyResponse{Body: jsonErr.Error(), StatusCode: http.StatusBadRequest}, nil
-	}
+	// get ids from pathParameters
+	groupID := request.PathParameters["groupId"]
+	userID := request.PathParameters["userId"]
 
 	// leave
-	leaveStatus, leaveErr := group.Leave(reqBody.UserID, reqBody.GroupID)
+	leaveStatus, leaveErr := group.Leave(userID, groupID)
 	if leaveErr != nil {
 		return events.APIGatewayProxyResponse{Body: leaveErr.Error(), StatusCode: leaveStatus}, nil
 	}
