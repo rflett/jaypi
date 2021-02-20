9e14ddb9d289044d6e156e98582e50a8109206f9
Ryan Flett <ryan.flett1@gmail.com>
2021-02-21 09:40:43 +1000

    Remove _archive and updated postman

--- _archive/types/guess/guess.go
+++
@@ -1,7 +0,0 @@
-package guess
-
-// Guess is a song guess consisting of an artist and song name
-type Guess struct {
-	Artist string `json:"artist"`
-	Song   string `json:"song"`
-}
--- _archive/types/song/song.go
+++
@@ -1,8 +0,0 @@
-package song
-
-// Song is a song that can be voted for in the hottest 100
-type Song struct {
-	ID     string
-	title  string
-	artist string
-}
