283a0e083ac58dba797b9a6cc51715b29c7dd17c
Ryan Flett <ryan.flett@hotmail.com>
2021-03-07 18:50:17 +1000

    Refactor chune-machine to use spotify song attributes, refactor to be better

--- source/lambda/chune-machine/main.go
+++ source/lambda/chune-machine/main.go
@@ -21,11 +21,17 @@ import (
 	"time"
 )
 
-const location = "Australia/Sydney"
+const tzLocation = "Australia/Sydney"
 
 var (
 	chuneRefreshQueue = os.Getenv("REFRESH_QUEUE")
 	beanCounterQueue  = os.Getenv("COUNTER_QUEUE")
+	config            = &clientcredentials.Config{
+		ClientID:     os.Getenv("SPOTIFY_CLIENT_ID"),
+		ClientSecret: os.Getenv("SPOTIFY_SECRET_ID"),
+		TokenURL:     spotify.TokenURL,
+	}
+	client = spotify.Client{}
 )
 
 // queueForCounter puts the songID on a queue to trigger the counter lambdas
@@ -53,8 +59,8 @@ func queueForCounter(songID *string) error {
 // queueForSelf puts the song information on the queue to re-trigger this lambda
 func queueForSelf(s *types.Song, nextUpdated *time.Time) error {
 	// now
-	l, _ := time.LoadLocation(location)
-	now := time.Now().In(l).UTC().Unix()
+	location, _ := time.LoadLocation(tzLocation)
+	now := time.Now().In(location).UTC().Unix()
 
 	// queue delay
 	diff := nextUpdated.Unix() - now
@@ -89,40 +95,40 @@ func queueForSelf(s *types.Song, nextUpdated *time.Time) error {
 
 // lookupSpotify queries Spotify for a song to obtain info like its ID and album art etc
 func lookupSpotify(s *types.Song) error {
-	// authenticate to spotify
-	config := &clientcredentials.Config{
-		ClientID:     os.Getenv("SPOTIFY_CLIENT_ID"),
-		ClientSecret: os.Getenv("SPOTIFY_SECRET_ID"),
-		TokenURL:     spotify.TokenURL,
-	}
-	token, err := config.Token(context.Background())
-	if err != nil {
-		logger.Log.Error().Err(err).Msg("Couldn't get spotify token")
-		return err
-	}
-	client := spotify.Authenticator{}.NewClient(token)
-
 	// search spotify for the track
 	logger.Log.Info().Str("track", s.SearchString()).Msg("Searching spotify for track")
-	results, searchErr := client.SearchOpt(s.SearchString(), spotify.SearchTypeTrack, &spotify.Options{
+	results, err := client.SearchOpt(s.SearchString(), spotify.SearchTypeTrack, &spotify.Options{
 		Limit: aws.Int(3),
 	})
-	if searchErr != nil {
+	if err != nil {
 		logger.Log.Error().Err(err).Str("track", s.SearchString()).Msg("Unable to search spotify for track")
-		return searchErr
+		return err
 	}
 
 	// search returned no results
-	if len(results.Tracks.Tracks) == 0 {
+	if results.Tracks.Total == 0 {
 		msg := "spotify search returned no results"
 		logger.Log.Warn().Str("track", s.SearchString()).Msg(msg)
 		return errors.New(msg)
 	}
 
-	// search returned results
-	songID := results.Tracks.Tracks[0].SimpleTrack.ID.String()
-	logger.Log.Info().Str("songID", songID).Msg("Found song on spotify!")
-	s.SongID = songID
+	// search returned results, update song with spotify info
+	s.SongID = results.Tracks.Tracks[0].SimpleTrack.ID.String()
+	s.Name = results.Tracks.Tracks[0].SimpleTrack.Name
+	s.Album = results.Tracks.Tracks[0].Album.Name
+	s.Artist = results.Tracks.Tracks[0].Artists[0].Name
+
+	// get the album artwork
+	var artwork []jjj.ArtworkSize
+	for _, art := range results.Tracks.Tracks[0].Album.Images {
+		artwork = append(artwork, jjj.ArtworkSize{
+			Url:    art.URL,
+			Width:  art.Width,
+			Height: art.Height,
+		})
+	}
+	s.Artwork = &artwork
+	logger.Log.Info().Str("songID", s.SongID).Msg("Found song on spotify!")
 
 	return nil
 }
@@ -132,11 +138,11 @@ func getNowPlaying() (*types.Song, *time.Time) {
 	logger.Log.Info().Msg("Checking JJJ for what's playing")
 
 	// query JJJ
-	nowPlaying, getNowErr := http.Get(
-		fmt.Sprintf("https://music.abcradio.net.au/api/v1/plays/triplej/now.json?tz=%s", location),
+	nowPlaying, err := http.Get(
+		fmt.Sprintf("https://music.abcradio.net.au/api/v1/plays/triplej/now.json?tz=%s", tzLocation),
 	)
-	if getNowErr != nil {
-		logger.Log.Error().Err(getNowErr).Msg("Couldn't get latest song")
+	if err != nil {
+		logger.Log.Error().Err(err).Msg("Couldn't get latest song")
 		return nil, nil
 	}
 
@@ -145,9 +151,9 @@ func getNowPlaying() (*types.Song, *time.Time) {
 	// unmarshal response
 	response := jjj.ResponseBody{}
 	bodyBytes, _ := ioutil.ReadAll(nowPlaying.Body)
-	jsonErr := json.Unmarshal(bodyBytes, &response)
-	if jsonErr != nil {
-		logger.Log.Error().Err(jsonErr).Msg("Unable to unmarshal JJJ response to jjjResponseBody")
+	err = json.Unmarshal(bodyBytes, &response)
+	if err != nil {
+		logger.Log.Error().Err(err).Msg("Unable to unmarshal JJJ response to jjjResponseBody")
 		return nil, nil
 	}
 
@@ -167,36 +173,27 @@ func getNowPlaying() (*types.Song, *time.Time) {
 	var playedAt string
 	playedTime, timeParseErr := time.Parse(time.RFC3339, response.Now.PlayedTime)
 	if timeParseErr != nil {
-		logger.Log.Warn().Msg("Unable to parse JJJ PlayedTime to RFC3339, using now instead")
+		logger.Log.Warn().Msg("Unable to parse JJJ PlayedTime to RFC3339, using current time instead")
 		playedAt = time.Now().UTC().Format(time.RFC3339)
 	} else {
 		playedAt = playedTime.Format(time.RFC3339)
-	}
-
-	// get artwork
-	var artwork []jjj.ArtworkSize
-	if len(response.Now.Release.Artwork) == 0 || response.Now.Release == nil {
-		artwork = nil
-	} else {
-		artwork = response.Now.Release.Artwork[0].Sizes
 	}
 
 	return &types.Song{
 		Name:     title,
 		Album:    response.Now.Release.Title,
 		Artist:   response.Now.Release.Artists[0].Name,
-		Artwork:  &artwork,
 		PlayedAt: &playedAt,
 	}, &nextUpdated
 }
 
 func HandleRequest(ctx context.Context, sqsEvent events.SQSEvent) error {
 	// unmarshall sqsEvent to messageBody
-	mb := types.ChuneRefreshBody{}
-	jsonErr := json.Unmarshal([]byte(sqsEvent.Records[0].Body), &mb)
-	if jsonErr != nil {
-		logger.Log.Error().Err(jsonErr).Msg("Unable to unmarshal sqsEvent body to messageBody struct")
-		return jsonErr
+	body := types.ChuneRefreshBody{}
+	err := json.Unmarshal([]byte(sqsEvent.Records[0].Body), &body)
+	if err != nil {
+		logger.Log.Error().Err(err).Msg("Unable to unmarshal sqsEvent body to messageBody struct")
+		return err
 	}
 
 	// get what's now playing on JJJ
@@ -204,24 +201,20 @@ func HandleRequest(ctx context.Context, sqsEvent events.SQSEvent) error {
 
 	// if no song is playing just put the song back on the queue with the same ID
 	if jjjSong == nil {
-		logger.Log.Info().Str("songID", mb.SongID).Msg("Putting song back on queue with updated delay as no new song is playing yet")
-		_ = queueForSelf(&types.Song{SongID: mb.SongID}, nextUpdated)
-		return nil
+		logger.Log.Info().Str("songID", body.SongID).Msg("Putting song back on queue with updated delay as no new song is playing yet")
+		return queueForSelf(&types.Song{SongID: body.SongID}, nextUpdated)
 	}
 
 	// lookup song on Spotify
-	spotifyLookupErr := lookupSpotify(jjjSong)
-	if spotifyLookupErr != nil {
-		logger.Log.Warn().Str("songID", mb.SongID).Msg("Putting song back on queue because we couldn't search for it on spotify")
-		_ = queueForSelf(&types.Song{SongID: mb.SongID}, nextUpdated)
-		return nil
+	if err = lookupSpotify(jjjSong); err != nil {
+		logger.Log.Warn().Str("songID", body.SongID).Msg("Putting song back on queue because we couldn't search for it on spotify")
+		return queueForSelf(&types.Song{SongID: body.SongID}, nextUpdated)
 	}
 
 	// if the same song is playing then come back later
-	if jjjSong.SongID == mb.SongID {
-		logger.Log.Info().Str("songID", mb.SongID).Msg("Putting song back on queue as it is still playing")
-		_ = queueForSelf(jjjSong, nextUpdated)
-		return nil
+	if jjjSong.SongID == body.SongID {
+		logger.Log.Info().Str("songID", body.SongID).Msg("Putting song back on queue as it is still playing")
+		return queueForSelf(&types.Song{SongID: body.SongID}, nextUpdated)
 	}
 
 	// add the song to the table if it doesn't exist
@@ -240,6 +233,11 @@ func HandleRequest(ctx context.Context, sqsEvent events.SQSEvent) error {
 	_ = queueForSelf(jjjSong, nextUpdated)
 
 	return nil
+}
+
+func init() {
+	token, _ := config.Token(context.Background())
+	client = spotify.Authenticator{}.NewClient(token)
 }
 
 func main() {
