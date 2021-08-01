a65bd59b457ed62678e9cc683dad3be2da18f592
Ryan Flett <ryan.flett@hotmail.com>
2021-08-01 13:52:02 +1000

    Fix response body

--- source/rest/song/getPlayedSongs/main.go
+++ source/rest/song/getPlayedSongs/main.go
@@ -4,8 +4,14 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"jjj.rflett.com/jjj-api/services"
+	"jjj.rflett.com/jjj-api/types"
 	"net/http"
 )
+
+
+type responseBody struct {
+	Songs []types.Song `json:"songs"`
+}
 
 // Handler is our handle on life
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
@@ -13,7 +19,10 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	if err != nil {
 		return services.ReturnError(err, http.StatusInternalServerError)
 	}
-	return services.ReturnJSON(recentSongs, http.StatusOK)
+
+	// return the songs
+	rb := responseBody{Songs: recentSongs}
+	return services.ReturnJSON(rb, http.StatusOK)
 }
 
 func main() {
